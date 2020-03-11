664c362f0dde26eaa9708d35090765563398eecc
theCodejugglers <codejugglers@gmail.com>
2020-03-11 17:06:01 +0100

    Make multilingual.

---
+++ resources/js/lang.js
@@ -0,0 +1,7 @@
+var lang1 = window.location.hostname.endsWith("de") ? "de" : "en";
+function activateLang(lang1) {
+	var lang2 = (lang1 === "de") ? "en" : "de";
+	$('*[lang="'+lang2+'"]').hide();
+	$('*[lang="'+lang1+'"]').show();
+}
+//activateLang(lang1);
